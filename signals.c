8e9f16fd5ea24bf74a6893107a869926b3aa00fb
moriaMaman <44782836+moriaMaman@users.noreply.github.com>
2019-05-29 22:04:42 +0300

    Update signals.c

--- signals.c
+++ signals.c
@@ -1,13 +1,3 @@
-// C program to illustrate 
-// pipe system call in C 
-// shared by Parent and Child 
-// #include <stdio.h> 
-// #include <string.h>
-// #include <stdlib.h>
-// #include <unistd.h> 
-// #include <sys/types.h>
-// #include <sys/wait.h>
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,85 +6,55 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
-#include <wait.h>
-#include <sys/wait.h>
 #include <sys/fcntl.h>
-#include "md5.h"
+#include <sys/wait.h>
+#include <stdbool.h>
 
-char  secondBuf[6000];
-int str_len=2;
-
-using namespace std;
+int j;
+int _chlids[5];
 
 
-void hendler(int signum){
-    str_len=strlen(secondBuf);
-    if(str_len==32){
-       printf("encrypted by process %d: %s\n",getpid(),secondBuf);
-       kill(0,SIGKILL);
-       exit(0);
-    }
-    else{
-        exit(1);
+void sigCather(int sig_num)
+{
+    signal(SIGTERM,sigCather);
+    printf("PID %d is caught one\n",getpid());
+    if(j >-1){
+        kill(_chlids[j],SIGTERM);
     }
 }
 
 int main() 
 { 
+pid_t pid;
+int zombi;
+int state;
 
+signal(SIGTERM, sigCather);
 
-    char  parentBuf[100];
-    char  firstBuf[300];
-    string md5Ans;
-    int myPipeFd1[2], pid, nbytes,myPipeFd2[2];
-
-    
-    
-    if (pipe(myPipeFd1) < 0){//בדיקה האם הפייפים נוצרו כמו שצריך
-        perror("first pipe failed");
-        exit(1); 
+for (int i = 0; i < 5; i++)
+{
+    pid = fork();
+    if(pid == 0){
+    //chlid Process
+    printf("PID %d is ready \n", getpid());
+    j = i-1;
+    pause(); // wait for singal
+    exit(0); // become a zombi
     }
-   if (pipe(myPipeFd2) < 0){//בדיקה האם הפייפים נוצרו כמו שצריך
-        perror("second pipe ailed");
-        exit(1); 
-    }
-  
-    /* continued */
-    pid=fork();
-    if(pid<0){//בדיקה האם התהליך נוצר כמו שצריך
-        perror("fork failed");
-        exit(1);
-    }
-
-    if (pid > 0) {//בדיקה האם התהליך הוא האבא או הבן
-      /*parent process*/
-      printf("plain input: ");
-      scanf("%s",parentBuf);
-      close(myPipeFd1[0]);
-      write(myPipeFd1[1],parentBuf,strlen(parentBuf)+1);
-      sleep (1);
-      close(myPipeFd2[1]);
-      read(myPipeFd2[0],secondBuf,128);
-      str_len=strlen(secondBuf);
-
-      kill(pid,SIGINT);
-      hendler(SIGINT);
-      exit(0);
-    } 
-  
-    else { 
-        /*child process*/
-        wait(NULL);
-        close(myPipeFd1[1]);
-        read(myPipeFd1[0],firstBuf,20);
-        md5Ans= md5(firstBuf);
-        char newChar[md5Ans.length()+1];
-        strcpy(newChar,md5Ans.c_str());
-        write(myPipeFd2[1],newChar,strlen(newChar)+1);
-        close(myPipeFd2[0]);
-        signal(SIGINT,hendler);
-        wait(NULL);
-        } 
-    return 0; 
+    // Parent Process cllect the chlids.
+    _chlids[i] = pid;
 }
+    sleep(1);
+    kill(_chlids[4],SIGTERM);
+    sleep(1);
+  
+    for (size_t i = 0; i < 5; i++)
+    {
+        zombi = wait(&state);
+        printf("process %d is dead\n",zombi);
+        kill(zombi,SIGKILL);
+    }
 
+
+    return 0;
+}
